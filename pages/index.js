6a63c4cc162084d8557ed5bf7b2384370eb49668
Dylan <dylanhudson1998@gmail.com>
2021-12-27 19:58:48 +0000

    Starting layout

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,15 @@
 import Head from 'next/head'
+import Navbar from '../components/navbar'
 import styles from '../styles/Home.module.css'
+import Footer from '../components/footer'
 
 export default function Home() {
   return (
     <div className={styles.container}>
-        <Head>
-         
-        </Head>
+      <Navbar/>
+      <Head>
+        
+      </Head>
       <main className={styles.main}>
         <h1 className={styles.title}>
           OKDPhotography
@@ -20,15 +23,7 @@ export default function Home() {
 
         </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        </a>
-      </footer>
+      <Footer/>
     </div>
   )
 }
